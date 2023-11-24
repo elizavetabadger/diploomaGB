1f7aef90c7e3594a5a57854522180e2cc10b4023
elizavetabadger <Artbel543@yandex.ru>
2023-11-24 18:04:04 +0300

    BudgetManagerApp_v1.0

--- src/main/java/Examples/BudgetManagerApp.java
+++ src/main/java/Examples/BudgetManagerApp.java
@@ -1,5 +1,14 @@
 package Examples;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PiePlot;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.general.PieDataset;
+
+import javax.swing.*;
 import java.sql.*;
 import java.util.Scanner;
 
@@ -18,7 +27,10 @@ public class BudgetManagerApp {
                 System.out.println("2. Добавить расход");
                 System.out.println("3. Показать список доходов");
                 System.out.println("4. Показать список расходов");
-                System.out.println("5. Выйти");
+                System.out.println("5. Удалить запись");
+                System.out.println("6. Рассчитать общий баланс");
+                System.out.println("7. Вывести диаграмму структурирования бюджета");
+                System.out.println("8. Выйти");
 
                 int choice = scanner.nextInt();
 
@@ -42,17 +54,81 @@ public class BudgetManagerApp {
                         listTransactions(connection, "Расход");
                         break;
                     case 5:
+                        System.out.print("Введите ID записи для удаления: ");
+                        int recordId = scanner.nextInt();
+                        deleteTransaction(connection, recordId);
+                        System.out.println("Запись удалена.");
+                        break;
+                    case 6:
+                        double balance = calculateBalance(connection);
+                        System.out.println("Общий баланс: " + balance);
+                        break;
+                    case 7:
+                        showBudgetStructureChart(connection);
+                        break;
+                    case 8:
                         System.out.println("Программа завершена.");
                         connection.close();
                         scanner.close();
                         return;
                     default:
-                        System.out.println("Неверный выбор. Пожалуйста, выберите действие от 1 до 5.");
+                        System.out.println("Неверный выбор. Пожалуйста, выберите действие от 1 до 8.");
                 }
             }
         } catch (SQLException e) {
             System.out.println("Ошибка при работе с базой данных: " + e.getMessage());
         }
+    }
+
+    private static void deleteTransaction(Connection connection, int recordId) throws SQLException {
+        String deleteSQL = "DELETE FROM transactions WHERE id = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(deleteSQL)) {
+            preparedStatement.setInt(1, recordId);
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    private static double calculateBalance(Connection connection) throws SQLException {
+        String selectIncomeSQL = "SELECT SUM(amount) FROM transactions WHERE type = 'Доход'";
+        String selectExpenseSQL = "SELECT SUM(amount) FROM transactions WHERE type = 'Расход'";
+        double income = 0.0;
+        double expense = 0.0;
+
+        try (Statement statement = connection.createStatement()) {
+            ResultSet incomeResult = statement.executeQuery(selectIncomeSQL);
+            if (incomeResult.next()) {
+                income = incomeResult.getDouble(1);
+            }
+
+            ResultSet expenseResult = statement.executeQuery(selectExpenseSQL);
+            if (expenseResult.next()) {
+                expense = expenseResult.getDouble(1);
+            }
+        }
+
+        return income - expense;
+    }
+
+    private static void showBudgetStructureChart(Connection connection) throws SQLException {
+        DefaultPieDataset dataset = new DefaultPieDataset();
+        String selectExpenseByTypeSQL = "SELECT type, SUM(amount) FROM transactions WHERE type = 'Расход' GROUP BY type";
+
+        try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(selectExpenseByTypeSQL);
+            while (resultSet.next()) {
+                String type = resultSet.getString(1);
+                double amount = resultSet.getDouble(2);
+                dataset.setValue(type, amount);
+            }
+        }
+
+        JFreeChart chart = ChartFactory.createPieChart("Структура бюджета", dataset, true, true, false);
+        PiePlot plot = (PiePlot) chart.getPlot();
+        JFrame frame = new JFrame("Диаграмма структурирования бюджета");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.getContentPane().add(new ChartPanel(chart));
+        frame.pack();
+        frame.setVisible(true);
     }
 
     private static void createTable(Connection connection) throws SQLException {
@@ -90,4 +166,3 @@ public class BudgetManagerApp {
         }
     }
 }
-
