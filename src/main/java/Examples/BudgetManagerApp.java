75cb20a308d1119fce1b4087a9a7a6202916fcb9
elizavetabadger <Artbel543@yandex.ru>
2023-11-24 18:31:03 +0300

    BudgetManagerApp_v1.2

--- src/main/java/Examples/BudgetManagerApp.java
+++ src/main/java/Examples/BudgetManagerApp.java
@@ -4,9 +4,7 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PiePlot;
-import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.general.DefaultPieDataset;
-import org.jfree.data.general.PieDataset;
 
 import javax.swing.*;
 import java.sql.*;
